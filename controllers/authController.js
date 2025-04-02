5c447556da67aeeb6e0a2bca03bdbe0697f59419
misung choi <tjd000110@gmail.com>
2025-04-02 23:38:34 +0900

    로그인 API 구현완료 - 이메일/비밀번호 검증 및 JWT 발급

--- controllers/authController.js
+++ controllers/authController.js
@@ -25,4 +25,34 @@ const registerUser = async (req, res) => {
     }
 }
 
-module.exports = { registerUser };
+//로그인 API 로직 작성
+
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        // email로 사용자 찾기
+        const user = await User.findOne({ email });
+        if(!user) return res.status(400).json({ message : '존재하지 않는 이메일입니다.' });
+
+        //비밀번호 비교
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) return res.status(400).json({ message: '비밀번호가 일치하지 않습니다.' });
+
+        //jwt 토큰 발급
+        const token = jwt.sign(
+            { userId: user._id }, //토큰에 사용자 ID 포함
+            process.env.JWT_SECRET, 
+            { expiresIn: '1h' } //토큰 만료시간 1
+        );
+
+        //응답
+        res.status(200).json({ message: '로그인 완료',token });
+
+    } catch (error) {
+        res.status(500).json({ message: '서버 오류가 발생했습니다.' });
+    }
+};
+
+
+module.exports = { registerUser, loginUser };
--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
-const connectDB = require('./config/db');
+const connectDB = require('./config/db'); //DB 분리 
+const authRoutes = require('./routes/auth'); //auth 라우트 추가
+
+require('dotenv').config(); //env 불러오기
 
 mongoose.set('strictQuery', true); //쿼리 오류방지 
-require('dotenv').config(); //env 불러오기
 
 connectDB(); //DB연결
 
-app.use(express.json());
+app.use(express.json()); //JSON 형태로 데이터 받기
 
+app.use('/auth', authRoutes); //auth 경로로 오는 요청은 authRoutes로 처리
 
 //기본 라우트
 app.get('/', (req,res) => {
